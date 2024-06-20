8dce0ae2ae6f25f175c155f51501536e5abed4f4
Ludovic Cleroux <ludydoo@gmail.com>
2024-06-20 17:14:36 +0200

    work

--- ui/src/api/config.ts
+++ ui/src/api/config.ts
@@ -1,7 +1,19 @@
 import { Configuration } from '@/api/gen'
 
+function getBasePath(): string {
+  let path = import.meta.env.VITE_API_BASE_PATH
+  if (!path) {
+    path =
+      window.location.protocol +
+      '//' +
+      window.location.hostname +
+      (window.location.port ? ':' + window.location.port : '')
+  }
+  return path
+}
+
 const config = new Configuration({
-  basePath: import.meta.env.VITE_API_BASE_PATH,
+  basePath: getBasePath(),
 })
 
 export default config
